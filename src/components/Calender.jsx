1c2b4e4a3950ae3b2c750d89707378bb6daa00be
melina-niavarani <91658429+melina-niavarani@users.noreply.github.com>
2023-11-14 21:09:13 +0330

    set calender

--- src/components/Calender.jsx
+++ src/components/Calender.jsx
@@ -1,8 +1,15 @@
 function Calender() {
+    const currentDate = () => {
+        const month = ["Jan","Feb","Mar","Apr","May","June","July","Aug","Sept","Oct","Nov","Dec"];
+        const current = new Date();
+        const date = `${current.getDate()}
+                    ${month[current.getMonth()]}`;
+        return date;
+    }
     return (
         <div className="info">
             <div className="date-holder">
-                <div className="date"></div>
+                <div className="date">{ currentDate() }</div>
             </div>
         </div>
     )
